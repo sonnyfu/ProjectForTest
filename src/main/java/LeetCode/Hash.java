e8d4564ad72c107ab4c61122482d5dfbef0ba621
sonny <sonny@192.168.1.100>
2019-08-27 00:35:52 +0800

    hash theory

---
+++ src/main/java/LeetCode/Hash.java
@@ -0,0 +1,36 @@
+package LeetCode;
+
+/**
+ * 
+ * @author sonny
+ *	
+ */
+public class Hash {
+
+	/**
+	 * 1 tow sum:
+	 * I:[1,-1,3,4]
+	 * 找出两数和为0的下标
+	 * O:
+	 * 解法：
+	 * 1.1 穷举:O(n^2),O(1)
+	 * 1.2 set:O(n),O(n)
+	 * 
+	 * 2 3 sum
+	 * I
+	 * O
+	 * 解法：
+	 * 2.1 传统hash+穷举，O(n^2)
+	 * 2.2 快排破坏原数据+hash+大小指针向中间挤
+	 * 
+	 * 3 4 sum
+	 * I
+	 * 
+	 * O
+	 * 解法：
+	 * 3.1 采用该数据结构并去重：hash<sum,hash<a,b>>
+	 * 
+	 */
+	
+	
+}
